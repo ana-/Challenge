807b780bf9a8858ccbe3d56f0db71be2dbd41efa
Ana Da hora <anadahora@Anas-MacBook-Pro.local>
2018-07-15 12:55:09 -0300

    projeto

---
+++ Yummy Time/Inicio.swift
@@ -0,0 +1,40 @@
+//
+//  Inicio.swift
+//  Yummy Time
+//
+//  Created by Nathalia Inacio on 08/07/2018.
+//  Copyright © 2018 Nathalia Inacio. All rights reserved.
+//
+
+import UIKit
+
+class Inicio: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func eatButton(_ sender: Any) {
+       
+    }
+    
+//    var performSegue(withIdentifier identifier: "goToCamera"",
+//        sender: Any?)
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Yummy Time/ViewController.swift
+++ Yummy Time/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.performSegue(withIdentifier: "inicio", sender: self)
+        })
+        
     }
+    
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
