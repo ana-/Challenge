ba982331ea62b9f1e09dc614d8868e0475b62f57
ana23new@gmail.com <anadahora@10-46-48-82.0200.prio.br.iosda.org>
2018-07-18 12:46:11 -0300

    commit 1

--- Yummy Time/ViewControllerDaHora.swift
+++ Yummy Time/ViewControllerDaHora.swift
@@ -10,7 +10,15 @@ import UIKit
 import AVKit
 import Vision
 
-class ViewControllerDaHora: UIViewController {
+
+class ViewControllerDaHora: UIViewController{
+    
+    
+   
+   
+
+    @IBOutlet weak var personagem: UIImageView!
+    var brocImages:[UIImage] = []
     
     //Declaring capture session
     let captureSession = AVCaptureSession()
@@ -28,7 +36,36 @@ class ViewControllerDaHora: UIViewController {
         //Camera starting function
         self.startingTheCam()
         
+        
+      
+//        personagem.frame = CGRect(x: 200, y: 150, width: 300, height: 300)
+        brocImages = createImageArray(total: 95, imagePrefix: "Screenshot")
+        animate(imageView: personagem, images: brocImages, positionX: 200,positionY: 150,width: 300,height:300)
     }
+    func createImageArray(total:Int,imagePrefix:String)->[UIImage]{
+        
+        var imageArray:[UIImage] = []
+        
+        for imageCount in 1...total {
+            let imageName = "\(imagePrefix)\(imageCount).png"
+            let image = UIImage(named: imageName)
+            imageArray.append(image!)
+        }
+        
+        
+        return imageArray
+    }//end createImageArray
+    
+
+    
+    func animate(imageView:UIImageView, images:[UIImage],positionX:Double,positionY:Double,width:Double,height:Double){
+        imageView.frame = CGRect(x: 200, y: 150, width: 300, height: 300)
+        imageView.animationImages = images
+        imageView.layer.zPosition = 10
+        imageView.animationDuration = 1.0
+        imageView.startAnimating()
+        
+    }//end animate
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -53,7 +90,7 @@ class ViewControllerDaHora: UIViewController {
         
         //Data output
         let cameraDataOutput = AVCaptureVideoDataOutput()
-        cameraDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "outputVideo"))
+        cameraDataOutput.setSampleBufferDelegate(self as! AVCaptureVideoDataOutputSampleBufferDelegate, queue: DispatchQueue(label: "outputVideo"))
         captureSession.addOutput(cameraDataOutput)
         
         //Construct a camera preview layer
@@ -71,53 +108,52 @@ class ViewControllerDaHora: UIViewController {
         
     }
     
-    
-    
+
+
 }
 
 
 extension ViewControllerDaHora: AVCaptureVideoDataOutputSampleBufferDelegate{
-    
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        
+
+
         //Get pixel buffer
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             return
-            
+
         }
-        
+
         //get model
         guard let resNetModel = try? VNCoreMLModel(for: ImageClassifier().model) else { return }
-        
+
         //Create a coreml request
         let requestCoreML = VNCoreMLRequest(model: resNetModel) { (vnReq, err) in
-            
+
             //handling error and request
-            
+
             DispatchQueue.main.async {
                 if err == nil{
-                    
-                    
-                    
-                    guard let capturedRes = vnReq.results as? [VNClassificationObservation] else { return }
-                    
-                    guard let firstObserved = capturedRes.first else { return }
-                    
-                    print(firstObserved.identifier, firstObserved.confidence)
-                    
-                    self.descLabel.text = String(format: "This may be %.2f%% %@", firstObserved.confidence, firstObserved.identifier)
-                    
-                }
-                
-            }
-            
-        }
-        
-        
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([requestCoreML])
-        
-    }
-    
-}
 
+
+
+                    guard let capturedRes = vnReq.results as? [VNClassificationObservation] else { return }
+
+                    guard let firstObserved = capturedRes.first else { return }
+
+                    print(firstObserved.identifier, firstObserved.confidence)
+
+                    self.descLabel.text = String(format: "This may be %.2f%% %@", firstObserved.confidence, firstObserved.identifier)
+
+                }
+
+            }
+
+        }
+
+
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([requestCoreML])
+
+    }
+
+}
